f398eaeed6e6318d168f518b54cd564313723654
Wangjunchen12138 <70731190+Wangjunchen12138@users.noreply.github.com>
2021-11-07 10:59:34 +0800

    Update emails.py

--- emails.py
+++ emails.py
@@ -7,7 +7,7 @@ from remain import remain
 
 # 发件人和收件人
 sender = ' '
-receiver = ' '
+receiver = ['']
 # 所使用的用来发送邮件的SMTP服务器
 smtpServer = ' '
 # 发送邮箱的用户名和授权码（不是登录邮箱的密码）
@@ -23,15 +23,21 @@ mail_body = news
 
 message = MIMEText(mail_body, 'plain', 'utf-8')  # 邮件正文
 message['From'] = sender  # 邮件上显示的发件人
-message['To'] = receiver  # 邮件上显示的收件人
+message['To'] = ','.join(receiver) # 邮件上显示的收件人
 message['Subject'] = Header(mail_title, 'utf-8')  # 邮件主题
 
-try:
-    smtp = smtplib.SMTP()  # 创建一个连接
-    smtp.connect(smtpServer)  # 连接发送邮件的服务器
-    smtp.login(username, password)  # 登录服务器
-    smtp.sendmail(sender, receiver, message.as_string())  # 填入邮件的相关信息并发送
-    print("邮件发送成功！！！")
-    smtp.quit()
-except smtplib.SMTPException:
-    print("邮件发送失败！！！")
+def running():
+    try:
+        smtp = smtplib.SMTP()  # 创建一个连接
+        smtp.connect(smtpServer)  # 连接发送邮件的服务器
+        smtp.login(username, password)  # 登录服务器
+        for name in receiver:
+            smtp.sendmail(sender, name, message.as_string())  # 填入邮件的相关信息并发送
+        print("邮件发送成功！！！")
+        smtp.quit()
+        exit()
+    except smtplib.SMTPException:
+        print("邮件发送失败！！！")
+        exit()
+
+running()
